fddd794b449fe5d3222fee0f7d3364fa0109fe9e
Paul Gierz <pgierz@awi.de>
2025-06-03 08:23:36 +0200

    doc: improve formatting for the file_native option documentation

--- src/pymor/core/config.py
+++ src/pymor/core/config.py
@@ -160,8 +160,8 @@ class PymorConfig:
             default="1YS",
             doc="""Default timespan for grouping output files together.
 
-            Use the special flag `file_native` to use the same grouping as in the input
-            files. Otherwise, use a pandas-flavoured string, see: https://tinyurl.com/38wxf8px
+            Use the special flag ``'file_native'`` to use the same grouping as in the input
+            files. Otherwise, use a ``pandas``-flavoured string, see: https://tinyurl.com/38wxf8px
             """,
             parser=str,
         )
