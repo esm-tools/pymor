ff1f7293a55b20d14183d4959b28295383373a34
Paul Gierz <pgierz@awi.de>
2025-11-26 15:21:17 +0100

    wip: guardrail import of accessor functionality

--- src/pycmor/__init__.py
+++ src/pycmor/__init__.py
@@ -1,10 +1,13 @@
 """pycmor - Makes CMOR Simple"""
 
 # Import module that registers all xarray accessors
-from . import accessors  # noqa: F401
-from . import (
-    _version,
-)
+try:
+    from .accessors import *  # noqa: F401, F403
+
+    _XARRAY_ACCESSOR_AVAILABLE = True
+except ImportError:
+    _XARRAY_ACCESSOR_AVAILABLE = False
+from . import _version
 
 __author__ = "Paul Gierz <pgierz@awi.de>"
 __all__ = []
