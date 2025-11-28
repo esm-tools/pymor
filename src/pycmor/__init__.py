9677fb9d2870245942f430f4a032ae83d2548d9c
Paul Gierz <pgierz@awi.de>
2025-11-28 15:32:47 +0100

    feat: tutorials

--- src/pycmor/__init__.py
+++ src/pycmor/__init__.py
@@ -8,9 +8,11 @@ try:
     _XARRAY_ACCESSOR_AVAILABLE = True
 except ImportError:
     _XARRAY_ACCESSOR_AVAILABLE = False
+# Import tutorial module for easy access to example datasets
+from . import tutorial  # noqa: F401
 from . import _version
 
 __author__ = "Paul Gierz <pgierz@awi.de>"
-__all__ = []
+__all__ = ["tutorial"]
 
 __version__ = _version.get_versions()["version"]
---
+++ src/pycmor/tutorial/datasets/__init__.py
@@ -0,0 +1,7 @@
+"""Tutorial datasets - model run fixtures for pycmor examples."""
+
+from .awicm_recom import AwicmRecomModelRun
+from .fesom_2p6 import Fesom2p6ModelRun
+from .fesom_dev import FesomDevModelRun
+
+__all__ = ["AwicmRecomModelRun", "Fesom2p6ModelRun", "FesomDevModelRun"]
