8fb27770a0ecfb9c257fac3f38894635aa5b29d0
Paul Gierz <pgierz@awi.de>
2026-04-01 13:23:22 +0200

    wip: better try to prefect checkpoints

--- src/pycmor/core/caching.py
+++ src/pycmor/core/caching.py
@@ -7,7 +7,6 @@ import json
 import pickle
 from pathlib import Path
 
-from prefect.context import get_run_context
 from prefect.states import Completed
 
 from .logging import logger
@@ -24,11 +23,10 @@ def generate_cache_key(task, inputs):
 def manual_checkpoint(data, rule):
     """Manually insert a checkpoint in the flow"""
     logger.info("Manually inserting checkpoint")
-    try:
-        get_run_context()
-        return Completed(message="Checkpoint reached", data=data)
-    except RuntimeError:
-        return data
+    for pipeline in rule.pipelines:
+        if getattr(pipeline, "_workflow_backend", None) == "prefect":
+            return Completed(message="Checkpoint reached", data=data)
+    return data
 
 
 def inspect_cache(cache_dir="~/.prefect/storage"):
