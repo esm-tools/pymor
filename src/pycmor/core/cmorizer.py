821f0ebb4ab540a6fc4a535f9a68f569cf390dde
Jan Streffing <jan.streffing@awi.de>
2026-05-13 23:10:20 +0200

    Revert "_process_rule: @task(retries=3, retry_delay_seconds=30)"
    This reverts commit 76b1d4784e6a798ed6c4be854413f7547d9f7d1e.

--- src/pycmor/core/cmorizer.py
+++ src/pycmor/core/cmorizer.py
@@ -1273,7 +1273,7 @@ class CMORizer:
         return data
 
     @staticmethod
-    @task(name="Process rule", retries=3, retry_delay_seconds=30)
+    @task(name="Process rule")
     def _process_rule(rule):
         logger.info(f"Starting to process rule {rule}")
         data = None
